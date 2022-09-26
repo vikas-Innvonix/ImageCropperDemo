7feb502fd6ff5cc4958c2e8e978381cfa6d2d429
vikas-Innvonix <vikas.innvonix@gmail.com>
2022-09-26 16:57:20 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
-
+import { NgxImageCompressService } from 'ngx-image-compress';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'image-crop-demo';
+  imgChangeEvent: any = '';
+  cropImgPreview: any = '';
+
+  image: string;
+
+  imgResultBeforeCompress: string = '';
+  imgResultAfterCompress: string = '';
+  constructor(private imageCompressor: NgxImageCompressService) {
+    this.image = '';
+  }
+  onFileChange(event: any): void {
+    this.imgChangeEvent = event;
+  }
+
+  cropImg(e: ImageCroppedEvent) {
+    this.cropImgPreview = e.base64;
+  }
+
+  imgLoad() {}
+  initCropper() {}
+  imgFailed() {}
+
+  formatSizeUnits(bytes: number): string {
+    if (bytes >= 1073741824) {
+      return (bytes / 1073741824).toFixed(2) + ' GB';
+    } else if (bytes >= 1048576) {
+      return (bytes / 1048576).toFixed(2) + ' MB';
+    } else if (bytes >= 1024) {
+      return (bytes / 1024).toFixed(2) + ' KB';
+    } else if (bytes > 1) {
+      return bytes + ' bytes';
+    } else if (bytes == 1) {
+      return bytes + ' byte';
+    } else {
+      return '0 bytes';
+    }
+  }
+
+  compressFile() {
+    this.imageCompressor.uploadFile().then(({ image, orientation }) => {
+      this.imgResultBeforeCompress = image;
+      console.log(
+        'Size in bytes of image upload is: ' +
+          this.formatSizeUnits(this.imageCompressor.byteCount(image))
+      );
+
+      this.imageCompressor
+        .compressFile(image, orientation, 50, 80)
+        .then((compress) => {
+          this.imgResultAfterCompress = compress;
+          console.log(
+            'Size in bytes of image after upload is: ' +
+              this.formatSizeUnits(this.imageCompressor.byteCount(compress))
+          );
+        });
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxImageCompressService } from 'ngx-image-compress';
+import { ImageCropperModule } from 'ngx-image-cropper';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, ImageCropperModule],
+  providers: [NgxImageCompressService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
